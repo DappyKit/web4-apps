7524671972e1c03afca8e05d795fb429213ff1fe
Igor Shadurin <igor.shadurin@gmail.com>
2025-03-05 17:53:43 +0300

    feat: update Prettier configuration and add legacy format files to .gitignore

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+} 
